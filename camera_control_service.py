a612bc0264ef12fa461391132dfbe5c23e4f1c6c
Mihaidono <amihai2001@gmail.com>
2023-11-14 17:30:39 +0200

    fixed errors in control and removed max_dbp global variable

--- camera_control_service.py
+++ camera_control_service.py
@@ -24,75 +24,126 @@ def detect_camera_movement() -> bool:
 
 def move_camera_left_5_degrees():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_left', 'degree': 5})
+                       json.dumps({'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_left',
+                                   'degree': 5}))
 
 
 def move_camera_right_5_degrees():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_right', 'degree': 5})
+                       json.dumps({'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_right',
+                                   'degree': 5}))
 
 
 def move_camera_up_5_degrees():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_up', 'degree': 5})
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_up',
+                            'degree': 5}))
 
 
 def move_camera_down_5_degrees():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_down', 'degree': 5})
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_down',
+                            'degree': 5}))
 
 
 def move_camera_left_10_degrees():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_left', 'degree': 10})
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_left',
+                            'degree': 10}))
 
 
 def move_camera_right_10_degrees():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_right',
-                        'degree': 10})
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                            'cmd': 'relmove_right',
+                            'degree': 10}))
 
 
 def move_camera_up_10_degrees():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_up', 'degree': 10})
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                            'cmd': 'relmove_up', 'degree': 10}))
 
 
 def move_camera_down_10_degrees():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_down', 'degree': 10})
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                            'cmd': 'relmove_down',
+                            'degree': 10}))
 
 
 def move_camera_left_20_degrees():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_left',
-                        'degree': 20})
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                            'cmd': 'relmove_left',
+                            'degree': 20}))
 
 
 def move_camera_right_20_degrees():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_right',
-                        'degree': 20})
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                            'cmd': 'relmove_right',
+                            'degree': 20}))
 
 
 def move_camera_up_20_degrees():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_up', 'degree': 20})
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                            'cmd': 'relmove_up',
+                            'degree': 20}))
 
 
 def move_camera_down_20_degrees():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'relmove_down', 'degree': 20})
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime(
+                               "%Y-%m-%dT%H:%M:%S.%fZ"),
+                               'cmd': 'relmove_down', 'degree': 20}))
+
+
+def move_camera_right_max():
+    client_txt.publish('o/ptu',
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime(
+                               "%Y-%m-%dT%H:%M:%S.%fZ"),
+                               'cmd': 'end_pan'}))
+
+
+def move_camera_left_max():
+    client_txt.publish('o/ptu',
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime(
+                               "%Y-%m-%dT%H:%M:%S.%fZ"),
+                               'cmd': 'start_pan'}))
 
 
 def set_camera_position_home():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'home'})
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime(
+                               "%Y-%m-%dT%H:%M:%S.%fZ"),
+                               'cmd': 'home'}))
 
 
 def set_camera_position_stop():
     client_txt.publish('o/ptu',
-                       {'ts': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), 'cmd': 'stop'})
+                       json.dumps(
+                           {'ts': datetime.utcnow().strftime(
+                               "%Y-%m-%dT%H:%M:%S.%fZ"),
+                               'cmd': 'stop'}))
+
+
+def set_camera_position_default():
+    pass
 
 
 def on_connect_txt(client, userdata, flags, rc):
@@ -105,10 +156,12 @@ def on_message_txt(client, userdata, msg):
     current_position = json.loads(msg.payload)
     if detect_camera_movement():
         previous_position = current_position
+    set_camera_position_default()
 
 
 txt_broker_address = os.getenv("TXT_CONTROLLER_ADDRESS")
-txt_topics_to_subscribe = os.getenv("TXT_CONTROLLER_SUBSCRIBED_TOPICS").split(',')
+txt_topics_to_subscribe = os.getenv(
+    "TXT_CONTROLLER_SUBSCRIBED_TOPICS").split(',')
 port_used = int(os.getenv("TXT_CONTROLLER_PORT_USED"))
 keep_alive = int(os.getenv("TXT_CONTROLLER_KEEP_ALIVE"))
 username = os.getenv('TXT_USERNAME')
@@ -120,7 +173,8 @@ client_txt.on_message = on_message_txt
 client_txt.username_pw_set(username=username, password=passwd)
 
 try:
-    client_txt.connect(host=txt_broker_address, port=port_used, keepalive=keep_alive, )
+    client_txt.connect(host=txt_broker_address, port=port_used,
+                       keepalive=keep_alive, )
     print("Successfully connected to TXT Controller")
     client_txt.loop_forever()
 except TimeoutError as ex:
