57c6a349bfd78e8754e0f4d5cd2fd6dc0b3ca731
Mihaidono <amihai2001@gmail.com>
2024-01-20 11:07:58 +0200

    completely remade using a class, hope it works, i need to test it

--- monitoring_service.py
+++ monitoring_service.py
@@ -24,188 +24,168 @@ class RoutineStatus:
     TIMED_OUT = 3
 
 
-camera_standby_timer = int(os.getenv("CAMERA_STANDBY_TIME"))
-standby_seconds_count = 0
+class MonitoringService:
+    def __init__(self):
+        self.detection_event = threading.Event()
+        self.camera_standby_timer = int(os.getenv("CAMERA_STANDBY_TIME"))
+        self.standby_seconds_count = 0
 
-detection_event = threading.Event()
+        self.prev_frame_with_detected_objects = []
+        self.reoccurrence_matrix = np.zeros((3, 3))
 
-previous_timestamp = datetime.utcnow()
+        self.json_mqtt_data = {}
+        self.previous_timestamp = datetime.utcnow()
 
-prev_frame_with_detected_objects = []
-reoccurrence_matrix = np.zeros((3, 3))
+        self.current_routine = RoutineStatus.INITIALIZING
+        self.client_txt_name = "MonitoringService"
+        self.txt_broker_address = os.getenv("TXT_CONTROLLER_ADDRESS")
+        self.port_used = int(os.getenv("TXT_CONTROLLER_PORT_USED"))
+        self.keep_alive = int(os.getenv("TXT_CONTROLLER_KEEP_ALIVE"))
+        self.username = os.getenv('TXT_USERNAME')
+        self.passwd = os.getenv('TXT_PASSWD')
 
-json_mqtt_data = {}
+    def initialization_routine(self):
+        self.initiate_camera_position()
+        self.prev_frame_with_detected_objects = []
+        self.current_routine = RoutineStatus.SURVEYING_BAY
 
-current_routine = RoutineStatus.INITIALIZING
+    def survey_bay_routine(self):
+        while True:
+            self.update_json_message()
+            if self.json_mqtt_data:
+                img = self.decode_image_from_base64()
+                if img is not None:
+                    coordinates_matrix = container_detector.identify_container_units(img)
+                    filled_coordinates_matrix = container_detector.get_missing_storage_spaces(coordinates_matrix)
+                    if self.has_container_moved(filled_coordinates_matrix):
+                        self.current_routine = RoutineStatus.SURVEYING_DELIVERY_PROCESS
+                        break
 
+    def camera_timeout_counter(self):
+        self.detection_event.set()
+        while self.detection_event.is_set():
+            self.standby_seconds_count += 1
+            time.sleep(1)
+            if self.standby_seconds_count >= self.camera_standby_timer:
+                self.standby_seconds_count = 0
+                self.detection_event.clear()
+                self.current_routine = RoutineStatus.TIMED_OUT
+                break
 
-def check_container_movement(prev_frame_workpiece, crt_frame_workpiece) -> bool:
-    if ((prev_frame_workpiece is dict and crt_frame_workpiece is not dict) or
-            (type(prev_frame_workpiece) is int and type(crt_frame_workpiece) is int
-             and crt_frame_workpiece == 0 and prev_frame_workpiece == 0)):
-        return True
-    return False
+    def survey_delivery_process_routine(self):
+        print("Starting surveillance of the in-delivery workpiece")
+        self.process_start_camera_position()
+        countdown_running = False
+        while True:
+            self.update_json_message()
+            if self.json_mqtt_data:
+                img = self.decode_image_from_base64()
+                if img is not None:
+                    detected_object = container_detector.identify_workpiece(img)
+                    if detected_object is None:
+                        if countdown_running is not True:
+                            threading.Thread(target=self.camera_timeout_counter, daemon=True).start()
+                            countdown_running = True
+                        continue
+                    self.standby_seconds_count = 0
+                    self.detection_event.clear()
+                    countdown_running = False
 
+                    crt_height, crt_width, _ = img.shape
+                    self.center_workpiece_in_frame(detected_object["coordinates"], img_width=crt_width,
+                                                   img_height=crt_height)
 
-def decode_image_from_base64(json_message: dict) -> cv2.typing.MatLike:
-    image_data = base64.b64decode(strip_encoded_image_data(json_message['data']))
-    image_np = np.frombuffer(image_data, np.uint8)
-    return cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+    def camera_timeout_routine(self):
+        print("Object lost from field of view. Returning to bay")
+        self.current_routine = RoutineStatus.INITIALIZING
 
+    def update_json_message(self):
 
-def strip_encoded_image_data(image_encoded_json_data: str) -> str:
-    return image_encoded_json_data.split(',', 1)[-1].strip()
+        msg = subscribe.simple("i/cam", hostname=self.txt_broker_address, port=self.port_used,
+                               client_id=self.client_txt_name,
+                               keepalive=self.keep_alive,
+                               auth={'username': self.username, 'password': self.passwd})
+        json_message = json.loads(msg.payload)
+        if "data" in json_message and "ts" in json_message:
+            current_timestamp = datetime.strptime(json_message["ts"], "%Y-%m-%dT%H:%M:%S.%fZ")
+            if self.previous_timestamp < current_timestamp:
+                self.json_mqtt_data = json_message
+                self.previous_timestamp = current_timestamp
 
-
-def has_container_moved(filled_coordinate_matrix: List[List]):
-    global prev_frame_with_detected_objects
-    global reoccurrence_matrix
-
-    if len(prev_frame_with_detected_objects) == 0:
-        prev_frame_with_detected_objects = filled_coordinate_matrix
+    @staticmethod
+    def check_container_movement(prev_frame_workpiece, crt_frame_workpiece) -> bool:
+        if ((prev_frame_workpiece is dict and crt_frame_workpiece is not dict) or
+                (type(prev_frame_workpiece) is int and type(crt_frame_workpiece) is int
+                 and crt_frame_workpiece == 0 and prev_frame_workpiece == 0)):
+            return True
         return False
 
-    for idx in range(0, len(filled_coordinate_matrix)):
-        for jdx in range(0, len(filled_coordinate_matrix)):
-            if check_container_movement(prev_frame_with_detected_objects[idx][jdx], filled_coordinate_matrix[idx][jdx]):
-                reoccurrence_matrix[idx][jdx] += 1
-            else:
-                reoccurrence_matrix[idx][jdx] = 0
+    def decode_image_from_base64(self) -> cv2.typing.MatLike:
+        image_data = base64.b64decode(self.json_mqtt_data['data'].split(',', 1)[-1].strip())
+        image_np = np.frombuffer(image_data, np.uint8)
+        return cv2.imdecode(image_np, cv2.IMREAD_COLOR)
 
-    prev_frame_with_detected_objects = filled_coordinate_matrix
-    if any(3 in column for column in reoccurrence_matrix):
-        reoccurrence_matrix = np.zeros((3, 3))
-        return True
-    return False
+    def has_container_moved(self, filled_coordinate_matrix: List[List]):
 
+        if len(self.prev_frame_with_detected_objects) == 0:
+            self.prev_frame_with_detected_objects = filled_coordinate_matrix
+            return False
 
-def center_workpiece_in_frame(workpiece_coordinates: tuple, img_width: float, img_height: float):
-    x_img_center_coord, y_img_center_coord = img_width / 2, img_height / 2
-    x_quarter_value = x_img_center_coord / 2
-    y_quarter_value = y_img_center_coord / 2
+        for idx in range(0, len(filled_coordinate_matrix)):
+            for jdx in range(0, len(filled_coordinate_matrix)):
+                if self.check_container_movement(self.prev_frame_with_detected_objects[idx][jdx],
+                                                 filled_coordinate_matrix[idx][jdx]):
+                    self.reoccurrence_matrix[idx][jdx] += 1
+                else:
+                    self.reoccurrence_matrix[idx][jdx] = 0
 
-    if x_img_center_coord + x_quarter_value > workpiece_coordinates[0] > x_img_center_coord - x_quarter_value and \
-            y_img_center_coord + y_quarter_value > workpiece_coordinates[1] > y_img_center_coord - y_quarter_value:
-        return
+        self.prev_frame_with_detected_objects = filled_coordinate_matrix
+        if any(3 in column for column in self.reoccurrence_matrix):
+            self.reoccurrence_matrix = np.zeros((3, 3))
+            return True
+        return False
 
-    if workpiece_coordinates[0] > x_img_center_coord + x_quarter_value:
-        camera_control.move_camera_right_10_degrees()
-    elif workpiece_coordinates[0] < x_img_center_coord - x_quarter_value:
-        camera_control.move_camera_left_20_degrees()
+    @staticmethod
+    def center_workpiece_in_frame(workpiece_coordinates: tuple, img_width: float, img_height: float):
+        x_img_center_coord, y_img_center_coord = img_width / 2, img_height / 2
+        x_quarter_value = x_img_center_coord / 2
+        y_quarter_value = y_img_center_coord / 2
 
-    if workpiece_coordinates[1] > y_img_center_coord + y_quarter_value:
-        camera_control.move_camera_down_20_degrees()
-    elif workpiece_coordinates[1] < y_img_center_coord - y_quarter_value:
-        camera_control.move_camera_up_10_degrees()
-    time.sleep(1)
+        if x_img_center_coord + x_quarter_value > workpiece_coordinates[0] > x_img_center_coord - x_quarter_value and \
+                y_img_center_coord + y_quarter_value > workpiece_coordinates[1] > y_img_center_coord - y_quarter_value:
+            return
 
+        if workpiece_coordinates[0] > x_img_center_coord + x_quarter_value:
+            camera_control.move_camera_right_10_degrees()
+        elif workpiece_coordinates[0] < x_img_center_coord - x_quarter_value:
+            camera_control.move_camera_left_20_degrees()
 
-def initiate_camera_position():
-    camera_control.set_camera_position_default()
-
-
-def process_start_camera_position():
-    camera_control.set_camera_position_to_process_start()
-
-
-def initialization_routine():
-    global current_routine
-    global prev_frame_with_detected_objects
-    initiate_camera_position()
-    prev_frame_with_detected_objects = []
-    current_routine = RoutineStatus.SURVEYING_BAY
-
-
-def survey_bay_routine():
-    global current_routine
-    while True:
-        update_json_message()
-        if json_mqtt_data:
-            img = decode_image_from_base64(json_mqtt_data)
-            if img is not None:
-                coordinates_matrix = container_detector.identify_container_units(img)
-                filled_coordinates_matrix = container_detector.get_missing_storage_spaces(coordinates_matrix)
-                if has_container_moved(filled_coordinates_matrix):
-                    current_routine = RoutineStatus.SURVEYING_DELIVERY_PROCESS
-                    break
-
-
-def camera_timeout_counter():
-    global current_routine
-    global standby_seconds_count
-
-    detection_event.set()
-    while detection_event.is_set():
-        standby_seconds_count += 1
+        if workpiece_coordinates[1] > y_img_center_coord + y_quarter_value:
+            camera_control.move_camera_down_20_degrees()
+        elif workpiece_coordinates[1] < y_img_center_coord - y_quarter_value:
+            camera_control.move_camera_up_10_degrees()
         time.sleep(1)
-        if standby_seconds_count >= camera_standby_timer:
-            standby_seconds_count = 0
-            detection_event.clear()
-            current_routine = RoutineStatus.TIMED_OUT
-            break
+
+    @staticmethod
+    def initiate_camera_position():
+        camera_control.set_camera_position_default()
+
+    @staticmethod
+    def process_start_camera_position():
+        camera_control.set_camera_position_to_process_start()
+
+    def start_monitoring(self):
+        routines = {
+            RoutineStatus.INITIALIZING: self.initialization_routine,
+            RoutineStatus.SURVEYING_BAY: self.survey_bay_routine,
+            RoutineStatus.SURVEYING_DELIVERY_PROCESS: self.survey_delivery_process_routine,
+            RoutineStatus.TIMED_OUT: self.camera_timeout_routine
+        }
+
+        while True:
+            routines[self.current_routine]()
 
 
-def survey_delivery_process_routine():
-    global current_routine
-    global standby_seconds_count
-
-    print("Starting surveillance of the in-delivery workpiece")
-    process_start_camera_position()
-    countdown_running = False
-    while True:
-        update_json_message()
-        if json_mqtt_data:
-            img = decode_image_from_base64(json_mqtt_data)
-            if img is not None:
-                detected_object = container_detector.identify_workpiece(img)
-                if detected_object is None:
-                    if countdown_running is not True:
-                        threading.Thread(target=camera_timeout_counter, daemon=True).start()
-                        countdown_running = True
-                    continue
-                standby_seconds_count = 0
-                detection_event.clear()
-                countdown_running = False
-
-                crt_height, crt_width, _ = img.shape
-                center_workpiece_in_frame(detected_object["coordinates"], img_width=crt_width, img_height=crt_height)
-
-
-def camera_timeout_routine():
-    global current_routine
-    print("Object lost from field of view. Returning to bay")
-    current_routine = RoutineStatus.INITIALIZING
-
-
-def update_json_message():
-    global previous_timestamp
-    global json_mqtt_data
-
-    msg = subscribe.simple("i/cam", hostname=txt_broker_address, port=port_used, client_id=client_txt_name,
-                           keepalive=keep_alive, auth={'username': username, 'password': passwd})
-    json_message = json.loads(msg.payload)
-    if "data" in json_message and "ts" in json_message:
-        current_timestamp = datetime.strptime(json_message["ts"], "%Y-%m-%dT%H:%M:%S.%fZ")
-        if previous_timestamp < current_timestamp:
-            json_mqtt_data = json_message
-            previous_timestamp = current_timestamp
-
-
-client_txt_name = "MonitoringService"
-txt_broker_address = os.getenv("TXT_CONTROLLER_ADDRESS")
-port_used = int(os.getenv("TXT_CONTROLLER_PORT_USED"))
-keep_alive = int(os.getenv("TXT_CONTROLLER_KEEP_ALIVE"))
-username = os.getenv('TXT_USERNAME')
-passwd = os.getenv('TXT_PASSWD')
-
-
-routines = {
-    RoutineStatus.INITIALIZING: initialization_routine,
-    RoutineStatus.SURVEYING_BAY: survey_bay_routine,
-    RoutineStatus.SURVEYING_DELIVERY_PROCESS: survey_delivery_process_routine,
-    RoutineStatus.TIMED_OUT: camera_timeout_routine
-}
-
-while True:
-    routines[current_routine]()
-    pass
+if __name__ == "__main__":
+    surveillance_system = MonitoringService()
+    surveillance_system.start_monitoring()
