ff32ccae10ce669cc7c363c3ad4be4e11b5aa0e8
Mihaidono <amihai2001@gmail.com>
2024-02-16 10:47:53 +0200

    added a few buttons and prepared axios for using the python api

--- Frontend/smart-monitoring-system-rui/src/App.js
+++ Frontend/smart-monitoring-system-rui/src/App.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faCog, faArrowAltCircleLeft, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
-import './App.css';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faCog,
+  faArrowAltCircleLeft,
+  faArrowAltCircleRight,
+  faArrowUp,
+  faArrowDown,
+  faArrowLeft,
+  faArrowRight,
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+} from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
 
 const AvailablePages = {
   HOME: 1,
@@ -16,35 +27,38 @@ function NavigationSideMenu({ setActivePage }) {
   };
 
   const goToHomePage = () => {
-    setActivePage(1);
-  }
+    setActivePage(AvailablePages.HOME);
+  };
 
   const goToSettingsPage = () => {
-    setActivePage(2);
-  }
-
+    setActivePage(AvailablePages.SETTINGS);
+  };
 
   return (
-    <div className={`navbar ${expanded ? 'expanded' : ''}`}>
+    <div className={`navbar ${expanded ? "expanded" : ""}`}>
       <li>
         <button onClick={goToHomePage}>
-          <div className='side-menu-button-content'>
-            <FontAwesomeIcon icon={faHome} size='2x' />
-            <p>{expanded ? "Home" : ''}</p>
+          <div className="side-menu-button-content">
+            <FontAwesomeIcon icon={faHome} size="2x" />
+            <p>{expanded ? "Home" : ""}</p>
           </div>
         </button>
       </li>
       <li>
         <button onClick={goToSettingsPage}>
-          <div className='side-menu-button-content'>
-            <FontAwesomeIcon icon={faCog} size='2x' />
-            <p>{expanded ? 'Settings' : ''}</p>
+          <div className="side-menu-button-content">
+            <FontAwesomeIcon icon={faCog} size="2x" />
+            <p>{expanded ? "Settings" : ""}</p>
           </div>
         </button>
       </li>
       <li>
         <button onClick={toggleNavbar}>
-          {expanded ? <FontAwesomeIcon icon={faArrowAltCircleLeft} size='2x' /> : <FontAwesomeIcon icon={faArrowAltCircleRight} size='2x' />}
+          {expanded ? (
+            <FontAwesomeIcon icon={faArrowAltCircleLeft} size="2x" />
+          ) : (
+            <FontAwesomeIcon icon={faArrowAltCircleRight} size="2x" />
+          )}
         </button>
       </li>
     </div>
@@ -53,7 +67,7 @@ function NavigationSideMenu({ setActivePage }) {
 
 function SettingsMenu() {
   return (
-    <div className='settings-main-container'>
+    <div className="settings-main-container">
       <p>Settings works!</p>
     </div>
   );
@@ -61,8 +75,31 @@ function SettingsMenu() {
 
 function HomePageMenu() {
   return (
-    <div className='home-main-container'>
+    <div className="home-main-container">
       <p>Home works!</p>
+      <div className="camera-buttons-container">
+        <button>
+          <FontAwesomeIcon icon={faArrowUp} size="2x" />
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faArrowDown} size="2x" />
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faArrowLeft} size="2x" />
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faArrowRight} size="2x" />
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faHome} size="2x" />
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faAngleDoubleLeft} size="2x" />
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faAngleDoubleRight} size="2x" />
+        </button>
+      </div>
     </div>
   );
 }
@@ -81,8 +118,8 @@ export default function App() {
     }
   };
   return (
-    <div className='base-container'>
-      <NavigationSideMenu setActivePage={setCurrentPage}/>
+    <div className="base-container">
+      <NavigationSideMenu setActivePage={setCurrentPage} />
       {renderActivePage()}
     </div>
   );
