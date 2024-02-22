fcd1d8ad7564b256b2c1232c7c586eeca849127d
Mihaidono <amihai2001@gmail.com>
2024-02-22 11:40:28 +0200

    added processes

--- Frontend/smart-monitoring-system-rui/src/components/ProcessOverview/ProcessOverview.js
+++ Frontend/smart-monitoring-system-rui/src/components/ProcessOverview/ProcessOverview.js
@@ -1,9 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTruck,
+  faWarehouse,
+  faIndustry,
+} from "@fortawesome/free-solid-svg-icons";
 import "./ProcessOverview.css";
 
 function ProcessOverview() {
+  const processes = {
+    1: <FontAwesomeIcon icon={faWarehouse} size="2x" />,
+    2: <FontAwesomeIcon icon={faIndustry} size="2x" />,
+    3: <FontAwesomeIcon icon={faTruck} size="2x" />,
+  };
   return (
     <div className="process-overview-container">
-      <p>nimic</p>
+      <div className="processes">
+        {Object.keys(processes).map((processKey) => (
+          <div className="process">{processes[processKey]}</div>
+        ))}
+      </div>
+      <div className="progress-arrow"></div>
     </div>
   );
 }
