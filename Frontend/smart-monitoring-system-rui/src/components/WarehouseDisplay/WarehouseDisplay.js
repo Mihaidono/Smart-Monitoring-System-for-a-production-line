7ab8d3d9245c2b7ae95bedecf63cdf7fb7799c6a
Mihaidono <amihai2001@gmail.com>
2024-02-22 16:15:22 +0200

    fixed warehouse display, now it displays rows and columns properly and added a label to each camera feed and warehouse display

--- Frontend/smart-monitoring-system-rui/src/components/WarehouseDisplay/WarehouseDisplay.js
+++ Frontend/smart-monitoring-system-rui/src/components/WarehouseDisplay/WarehouseDisplay.js
@@ -8,58 +8,65 @@ import { faHockeyPuck } from "@fortawesome/free-solid-svg-icons";
 const baseUrl = `http://${process.env.REACT_APP_BACKEND_API_BASE_URL}:${process.env.REACT_APP_BACKEND_API_PORT}`;
 
 function WarehouseDisplay() {
-  //const [warehouseStock, setWarehouseStock] = useState([]);
-  //const sentError = useRef(false);
-  const warehouseStock = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ];
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get(`${baseUrl}/get_warehouse_inventory`);
+  const [warehouseStock, setWarehouseStock] = useState([]);
+  const sentError = useRef(false);
 
-  //       const containersData = [];
-  //       response.data.containers.forEach((element) => {
-  //         const innerList = element.map((container) => {
-  //           return new WarehouseContainerDTO(
-  //             container.coordinates,
-  //             container.color,
-  //             container.type
-  //           );
-  //         });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/get_warehouse_inventory`);
 
-  //         containersData.push(innerList);
-  //       });
-  //       setWarehouseStock(containersData);
-  //       sentError.current = false;
-  //     } catch (error) {
-  //       if (!sentError.current) {
-  //         console.error("Error fetching warehouse inventory:", error);
-  //         sentError.current = true;
-  //       }
-  //     }
-  //   };
+        const containersData = [];
+        for (let i = 0; i < response.data.containers[0].length; i++) {
+          const innerList = response.data.containers.map((element) => {
+            return new WarehouseContainerDTO(
+              element[i].coordinates,
+              element[i].color,
+              element[i].type
+            );
+          });
+          containersData.push(innerList);
+        }
+        setWarehouseStock(containersData);
+        sentError.current = false;
+      } catch (error) {
+        if (!sentError.current) {
+          console.error("Error fetching warehouse inventory:", error);
+          sentError.current = true;
+        }
+      }
+    };
 
-  //   const interval = setInterval(fetchData, 1000);
+    const interval = setInterval(fetchData, 1000);
 
-  //   return () => clearInterval(interval);
-  // }, [warehouseStock]);
+    return () => clearInterval(interval);
+  }, [warehouseStock]);
 
   return (
     <div className="warehouse-display">
-      {warehouseStock.map((row, rowIndex) =>
-        row.map((element, columnIndex) => (
-          <div className={`grid-item row-${rowIndex}-col-${columnIndex}`}>
-            <FontAwesomeIcon
-              className="warehouse-container-icon"
-              icon={faHockeyPuck} size="5x"
-            />
-            <p className="warehouse-container-coordinates-label">{element}</p>
-          </div>
-        ))
-      )}
+      <h2 className="warehouse-display-label">High-Bay Warehouse</h2>
+      <div className="warehouse-grid">
+        {warehouseStock.map((row) =>
+          row.map((element) => (
+            <div className="warehouse-container">
+              <FontAwesomeIcon
+                className="warehouse-container-icon"
+                icon={faHockeyPuck}
+                size="4x"
+                style={{ color: element.color }}
+              />
+              <p className="warehouse-container-coordinates-label">
+                {element.coordinates &&
+                element.coordinates[0] !== undefined &&
+                element.coordinates[1] !== undefined
+                  ? `x: ${element.coordinates[0].toFixed(0)},
+                   y: ${element.coordinates[1].toFixed(0)}`
+                  : "Missing"}
+              </p>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
