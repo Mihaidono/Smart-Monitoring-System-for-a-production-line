55d89549a166a6e94fe4dfc665bd5cb158f386db
Mihaidono <amihai2001@gmail.com>
2024-02-27 11:36:15 +0200

    attempting the circular shape

--- Frontend/smart-monitoring-system-rui/src/components/ProcessOverview/ProcessOverview.js
+++ Frontend/smart-monitoring-system-rui/src/components/ProcessOverview/ProcessOverview.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FactoryIcon from "@mui/icons-material/Factory";
 import CallSplitIcon from "@mui/icons-material/CallSplit";
 import PrecisionManufacturingIcon from "@mui/icons-material/PrecisionManufacturing";
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import WarehouseIcon from "@mui/icons-material/Warehouse";
 import "./ProcessOverview.css";
+import axios from "axios";
+
+function determineActiveStep(data) {}
 
 function ProcessOverview() {
+  const [activeStep, setActiveStep] = useState(0);
+
   const icons = [
-    { icon: <FactoryIcon />, label: "Warehouse" },
-    { icon: <PrecisionManufacturingIcon />, label: "Processing Station" },
+    { icon: <WarehouseIcon />, label: "Warehouse" },
+    { icon: <FactoryIcon />, label: "Processing Station" },
     { icon: <CallSplitIcon />, label: "Sorting Line" },
     { icon: <LocalShippingIcon />, label: "Delivery" },
+    { icon: <PrecisionManufacturingIcon />, label: "Crane" },
   ];
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("your-endpoint");
+
+        const data = response.data;
+        const newActiveStep = determineActiveStep(data);
+
+        setActiveStep(newActiveStep);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      {icons.map((element) => {
-        return (
-          <div>
-            {element.icon}
-            {element.label}
+    <div className="process-overview-container">
+      <div className="circle"></div>
+      <div className="overview-circular-container">
+        {icons.map((step, index) => (
+          <div id={`step-item-${index}`} className="overview-step-container">
+            <div className="step-item-icon">{step.icon}</div>
+            <p className="step-item-label">{step.label}</p>
           </div>
-        );
-      })}
+        ))}
+      </div>
     </div>
   );
 }
