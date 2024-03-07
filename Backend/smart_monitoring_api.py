727cc8aaad49e95e6f0d383f9e41fa53ffa7072e
Mihaidono <amihai2001@gmail.com>
2024-03-07 23:19:59 +0200

    prepared for using websockets must test now

--- Backend/smart_monitoring_api.py
+++ Backend/smart_monitoring_api.py
@@ -1,7 +1,8 @@
+import json
 from threading import Thread
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
@@ -28,16 +29,12 @@ class CameraControlMessage(BaseModel):
     direction: str
 
 
-def start_uvicorn():
-    import uvicorn
-
-    uvicorn.run(smart_monitoring_app, host='0.0.0.0', port=8000)
-
-
 @smart_monitoring_app.post("/move_camera")
 async def move_camera(control_message: CameraControlMessage):
     try:
-        camera_control.move_camera(direction=control_message.direction, degrees=control_message.degrees)
+        camera_control.move_camera(
+            direction=control_message.direction, degrees=control_message.degrees
+        )
     except Exception as e:
         error_details = {
             "error_type": e.__class__.__name__,
@@ -49,34 +46,84 @@ async def move_camera(control_message: CameraControlMessage):
     return JSONResponse(content=response, status_code=200)
 
 
-@smart_monitoring_app.get('/get_image')
-async def get_image():
-    response = {"data": surveillance_system.json_mqtt_data['data']}
-    return JSONResponse(content=response, status_code=200)
+@smart_monitoring_app.websocket("/ws_get_image")
+async def get_image(websocket: WebSocket):
+    try:
+        await websocket.accept()
+
+        while True:
+            data = {"data": surveillance_system.json_mqtt_data["data"]}
+            await websocket.send_json(data)
+    except Exception as e:
+        error_details = {
+            "error_type": e.__class__.__name__,
+            "error_message": str(e),
+            "additional_info": "An unexpected error occurred in the WebSocket Image endpoint",
+        }
+        await websocket.send_json({"error": error_details})
 
 
-@smart_monitoring_app.get('/get_warehouse_inventory')
-async def get_warehouse_inventory():
-    response = {"containers": surveillance_system.warehouse_containers}
-    return JSONResponse(content=response, status_code=200)
+@smart_monitoring_app.websocket("/ws_get_warehouse_inventory")
+async def get_warehouse_inventory(websocket: WebSocket):
+    try:
+        await websocket.accept()
+
+        while True:
+            data = {"containers": surveillance_system.warehouse_containers}
+            await websocket.send_text(json.dumps(data))
+    except Exception as e:
+        error_details = {
+            "error_type": e.__class__.__name__,
+            "error_message": str(e),
+            "additional_info": "An unexpected error occurred in the WebSocket Warehouse endpoint",
+        }
+        await websocket.send_json({"error": error_details})
 
 
-@smart_monitoring_app.get('/get_current_module')
-async def get_current_module():
-    response = {"current_module": camera_control.current_module}
-    return JSONResponse(content=response, status_code=200)
+@smart_monitoring_app.websocket("/ws_get_current_module")
+async def get_current_module(websocket: WebSocket):
+    try:
+        await websocket.accept()
+
+        while True:
+            data = {"current_module": camera_control.current_module}
+            await websocket.send_text(json.dumps(data))
+    except Exception as e:
+        error_details = {
+            "error_type": e.__class__.__name__,
+            "error_message": str(e),
+            "additional_info": "An unexpected error occurred in the WebSocket Module endpoint",
+        }
+        await websocket.send_json({"error": error_details})
 
 
-@smart_monitoring_app.get('/get_process_state')
-async def get_process_state():
-    response = {"started": surveillance_system.process_started}
-    return JSONResponse(content=response, status_code=200)
+@smart_monitoring_app.websocket("/ws_get_process_state")
+async def get_process_state(websocket: WebSocket):
+    try:
+        await websocket.accept()
+
+        while True:
+            data = {"started": surveillance_system.process_started}
+            await websocket.send_text(json.dumps(data))
+    except Exception as e:
+        error_details = {
+            "error_type": e.__class__.__name__,
+            "error_message": str(e),
+            "additional_info": "An unexpected error occurred in the WebSocket Image endpoint",
+        }
+        await websocket.send_json({"error": error_details})
+
+
+def start_uvicorn():
+    import uvicorn
+
+    uvicorn.run(smart_monitoring_app, host="0.0.0.0", port=8000)
 
 
 if __name__ == "__main__":
-    camera_control.start_camera_control_service()
+    #camera_control.start_camera_control_service()
 
     uvicorn_thread = Thread(target=start_uvicorn)
     uvicorn_thread.start()
 
-    surveillance_system.start_monitoring()
+    #surveillance_system.start_monitoring()
