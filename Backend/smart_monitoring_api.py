1bcdf271343c0c640eaf45eb2bffe1e23d0c3bd5
Mihaidono <amihai2001@gmail.com>
2024-03-12 19:38:31 +0200

    main thread runs monitoring

--- Backend/smart_monitoring_api.py
+++ Backend/smart_monitoring_api.py
@@ -140,5 +140,4 @@ if __name__ == "__main__":
     uvicorn_thread = Thread(target=start_uvicorn, daemon=True)
     uvicorn_thread.start()
 
-    smart_monitoring_thread = Thread(target=surveillance_system.start_monitoring)
-    smart_monitoring_thread.start()
+    surveillance_system.start_monitoring()
