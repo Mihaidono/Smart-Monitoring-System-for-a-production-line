a464efda7eaa7a23610a77ea5c262b693b218a0d
Mihaidono <amihai2001@gmail.com>
2024-04-03 19:11:45 +0300

    added start for collapse with details for log messages

--- Frontend/smart-monitoring-system-rui/src/components/MonitoringLog/MonitoringLog.js
+++ Frontend/smart-monitoring-system-rui/src/components/MonitoringLog/MonitoringLog.js
@@ -1,4 +1,4 @@
-import { Grid, Typography, ButtonBase } from "@mui/material";
+import { Grid, Typography, ButtonBase, Collapse, Stack } from "@mui/material";
 import "./MonitoringLog.css";
 import React, { useState } from "react";
 import WarningAmberOutlinedIcon from "@mui/icons-material/WarningAmberOutlined";
@@ -13,112 +13,134 @@ function MonitoringLog({ logData }) {
   };
 
   return (
-    <ButtonBase
-      onClick={toggleVisivility}
+    <Stack
       sx={{
         width: "100%",
-        border: "2px solid var(--mainColor)",
-        borderRadius: 5,
       }}
     >
-      <Grid
-        container
+      <ButtonBase
+        onClick={toggleVisivility}
         sx={{
-          padding: "10px",
+          width: "100%",
+          border: "2px solid var(--mainColor)",
+          borderRadius: 5,
         }}
       >
         <Grid
-          xs={1}
-          item
           container
           sx={{
-            justifyContent: "center",
-            alignContent: "center",
+            padding: "10px",
           }}
         >
-          <WarningAmberOutlinedIcon
-            sx={{
-              fontSize: { xs: "32px", sm: "42px" },
-            }}
-          />
-        </Grid>
-        <Grid item container xs={11}>
           <Grid
+            xs={1}
             item
             container
-            xs={6}
-            md={3}
-            lg={3}
-            sx={{
-              justifyContent: { xs: "flex-start" },
-              alignContent: "center",
-              width: { xs: "100%", sm: "200px" },
-            }}
-          >
-            <Typography
-              noWrap
-              variant="body2"
-              sx={{
-                paddingLeft: "10px",
-                paddingRight: "10px",
-                color: "var(--secondaryColor)",
-              }}
-            >
-              {logData.timestamp}
-            </Typography>
-          </Grid>
-          <Grid
-            item
-            container
-            xs={12}
-            md={5}
-            lg={6}
-            order={{ xs: 3, md: 2 }}
             sx={{
               justifyContent: "center",
               alignContent: "center",
-              width: { xs: "100%", sm: "200px" },
             }}
           >
-            <Typography
-              noWrap
+            <WarningAmberOutlinedIcon
               sx={{
-                paddingLeft: "10px",
-                paddingRight: "10px",
-                color: "var(--mainColor)",
+                fontSize: { xs: "32px", sm: "42px" },
               }}
-            >
-              {logData.message}
-            </Typography>
+            />
           </Grid>
-          <Grid
-            item
-            container
-            xs={6}
-            md={4}
-            lg={3}
-            order={{ xs: 2, md: 3 }}
-            sx={{
-              justifyContent: { xs: "flex-start", md: "center" },
-              alignContent: "center",
-              color: "var(--defaultStateColor)",
-              width: { xs: "100%", sm: "200px" },
-            }}
-          >
-            <Typography
-              noWrap
+          <Grid item container xs={11}>
+            <Grid
+              item
+              container
+              xs={6}
+              md={3}
+              lg={3}
               sx={{
-                paddingLeft: "10px",
-                paddingRight: "10px",
+                justifyContent: { xs: "flex-start" },
+                alignContent: "center",
+                width: { xs: "100%", sm: "200px" },
               }}
-              variant="body2"
             >
-              Id: {logData.id}
-            </Typography>
+              <Typography
+                noWrap
+                variant="body2"
+                sx={{
+                  paddingLeft: "10px",
+                  paddingRight: "10px",
+                  color: "var(--secondaryColor)",
+                }}
+              >
+                {logData.timestamp}
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              container
+              xs={12}
+              md={5}
+              lg={6}
+              order={{ xs: 3, md: 2 }}
+              sx={{
+                justifyContent: "center",
+                alignContent: "center",
+                width: { xs: "100%", sm: "200px" },
+              }}
+            >
+              <Typography
+                noWrap
+                sx={{
+                  paddingLeft: "10px",
+                  paddingRight: "10px",
+                  color: "var(--mainColor)",
+                }}
+              >
+                {logData.message}
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              container
+              xs={6}
+              md={4}
+              lg={3}
+              order={{ xs: 2, md: 3 }}
+              sx={{
+                justifyContent: { xs: "flex-start", md: "center" },
+                alignContent: "center",
+                color: "var(--defaultStateColor)",
+                width: { xs: "100%", sm: "200px" },
+              }}
+            >
+              <Typography
+                noWrap
+                sx={{
+                  paddingLeft: "10px",
+                  paddingRight: "10px",
+                }}
+                variant="body2"
+              >
+                Id: {logData.id}
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
+      </ButtonBase>
+      <Grid container justifyContent="center" alignItems="center">
+        <Collapse
+          in={detailsVisible}
+          sx={{
+            width: "95%",
+            justifyContent: "center",
+            borderLeft: "2px solid var(--mainColor)",
+            borderRight: "2px solid var(--mainColor)",
+            borderBottom: "2px solid var(--mainColor)",
+          }}
+        >
+          <Grid container justifyContent="center" alignItems="center">
+            <Typography variant="body2">Additional details</Typography>
+          </Grid>
+        </Collapse>
       </Grid>
-    </ButtonBase>
+    </Stack>
   );
 }
 
