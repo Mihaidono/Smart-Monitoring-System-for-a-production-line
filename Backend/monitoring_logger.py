44270d7f4f58606a3289e3d4a12007be8b0a4a00
Mihaidono <amihai2001@gmail.com>
2024-04-10 11:58:54 +0300

    logs in timeframe and query logs were similar so i combined them

--- Backend/monitoring_logger.py
+++ Backend/monitoring_logger.py
@@ -25,11 +25,15 @@ class LogSeverity(Enum):
 
 
 class MonitoringLogMessage:
-    def __init__(self, message: str, severity: LogSeverity, while_tracking=None,
-                 current_module=None,
-                 current_routine=None,
-                 additional_data: dict | List = None
-                 ):
+    def __init__(
+        self,
+        message: str,
+        severity: LogSeverity,
+        while_tracking=None,
+        current_module=None,
+        current_routine=None,
+        additional_data: dict | List = None,
+    ):
         self._id = ObjectId()
         self._timestamp = datetime.utcnow()
         self._message = message
@@ -41,30 +45,34 @@ class MonitoringLogMessage:
 
     def get_log_data(self) -> dict:
         return {
-            '_id': self._id,
-            'timestamp': self._timestamp,
-            'while_tracking': self._while_tracking,
-            'current_routine': self._current_routine,
-            'current_module': self._current_module,
-            'message': self._message,
-            'additional_data': self._additional_data,
-            'severity': self._severity.value
+            "_id": self._id,
+            "timestamp": self._timestamp,
+            "while_tracking": self._while_tracking,
+            "current_routine": self._current_routine,
+            "current_module": self._current_module,
+            "message": self._message,
+            "additional_data": self._additional_data,
+            "severity": self._severity.value,
         }
 
     def log_data_to_json(self):
         return json.dumps(self.get_log_data())
 
     def __str__(self):
-        return f"{self.__class__.__name__}(id={self._id}, timestamp={self._timestamp}, message={self._message}, " \
-               f"additional_data={self._additional_data}, severity={self._severity.name}, " \
-               f"while_tracking={self._while_tracking}, " \
-               f"current_module={self._current_module}, current_routine={self._current_routine})"
+        return (
+            f"{self.__class__.__name__}(id={self._id}, timestamp={self._timestamp}, message={self._message}, "
+            f"additional_data={self._additional_data}, severity={self._severity.name}, "
+            f"while_tracking={self._while_tracking}, "
+            f"current_module={self._current_module}, current_routine={self._current_routine})"
+        )
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(id={self._id}, timestamp={self._timestamp}, message={self._message}, " \
-               f"additional_data={self._additional_data}, severity={self._severity.name}, " \
-               f"while_tracking={self._while_tracking}, " \
-               f"current_module={self._current_module}, current_routine={self._current_routine})"
+        return (
+            f"{self.__class__.__name__}(id={self._id}, timestamp={self._timestamp}, message={self._message}, "
+            f"additional_data={self._additional_data}, severity={self._severity.name}, "
+            f"while_tracking={self._while_tracking}, "
+            f"current_module={self._current_module}, current_routine={self._current_routine})"
+        )
 
 
 class MonitoringLogger:
@@ -81,82 +89,54 @@ class MonitoringLogger:
         return self._collection.count_documents({})
 
     def get_page_of_logs(self, current_page: int, limitation: int):
-        return self._collection.find({}).skip((current_page - 1) * limitation).limit(limitation)
+        return (
+            self._collection.find({})
+            .skip((current_page - 1) * limitation)
+            .limit(limitation)
+        )
 
-    def get_logs(self, log_id: ObjectId = None, message: str = None, severity: LogSeverity = None,
-                while_tracking: bool = None,
-                current_routine=None,
-                current_module=None) -> List:
+    def get_logs(
+        self,
+        log_id: ObjectId = None,
+        message: str = None,
+        severity: LogSeverity = None,
+        while_tracking: bool = None,
+        current_routine=None,
+        current_module=None,
+        lower_boundary: datetime = None,
+        upper_boundary: datetime = None,
+    ) -> List:
         query = {}
         if log_id is not None:
-            query['_id'] = log_id
+            query["_id"] = log_id
         if severity is not None:
-            query['severity'] = severity.value
+            query["severity"] = severity.value
         if while_tracking is not None:
-            query['while_tracking'] = while_tracking
+            query["while_tracking"] = while_tracking
         if current_routine is not None:
-            query['current_routine'] = current_routine
+            query["current_routine"] = current_routine
         if current_module is not None:
-            query['current_module'] = current_module
+            query["current_module"] = current_module
         if message is not None:
-            query['message'] = {'$regex': message}
+            query["message"] = {"$regex": message}
+        if lower_boundary is not None and upper_boundary is not None:
+            query["timestamp"] = {"$gte": lower_boundary, "$lte": upper_boundary}
+
         log_messages = []
         try:
             cursor = self._collection.find(query)
             for log in cursor:
-                log_message = MonitoringLogMessage(message=log["message"],
-                                                   while_tracking=log['while_tracking'],
-                                                   severity=LogSeverity.from_value(log['severity']),
-                                                   current_module=log['current_module'],
-                                                   current_routine=log['current_routine'],
-                                                   additional_data=log['additional_data']
-                                                   )
+                log_message = MonitoringLogMessage(
+                    message=log["message"],
+                    while_tracking=log["while_tracking"],
+                    severity=LogSeverity.from_value(log["severity"]),
+                    current_module=log["current_module"],
+                    current_routine=log["current_routine"],
+                    additional_data=log["additional_data"],
+                )
                 log_message._timestamp = log["timestamp"]
                 log_message._id = log["_id"]
                 log_messages.append(log_message)
         except Exception as e:
             print(e)
         return log_messages
-
-    def get_logs_in_timeframe(self, lower_boundary: datetime, upper_boundary: datetime):
-        query = {
-            'timestamp': {
-                '$gte': lower_boundary,
-                '$lte': upper_boundary
-            }
-        }
-        log_messages = []
-        try:
-            cursor = self._collection.find(query)
-            for log in cursor:
-                log_message = MonitoringLogMessage(message=log["message"],
-                                                   while_tracking=log['while_tracking'],
-                                                   severity=LogSeverity.from_value(log['severity']),
-                                                   current_module=log['current_module'],
-                                                   current_routine=log['current_routine'],
-                                                   additional_data=log['additional_data']
-                                                   )
-                log_message._timestamp = log["timestamp"]
-                log_message._id = log["_id"]
-                log_messages.append(log_message)
-        except Exception as e:
-            print(e)
-        return log_messages
-
-    def get_logs_in_week(self, date_in_week: datetime):
-        start_of_week = date_in_week - timedelta(days=date_in_week.weekday())
-        end_of_week = start_of_week + timedelta(days=6)
-
-        return self.get_logs_in_timeframe(start_of_week, end_of_week)
-
-    def get_logs_in_month(self, year: int = None, month: int = None):
-        if year is None:
-            year = datetime.utcnow().year
-        if month is None:
-            month = datetime.utcnow().month
-
-        start_of_month = datetime(year, month, 1)
-        next_month = start_of_month.replace(month=start_of_month.month + 1)
-        end_of_month = next_month - timedelta(days=1)
-
-        return self.get_logs_in_timeframe(start_of_month, end_of_month)
