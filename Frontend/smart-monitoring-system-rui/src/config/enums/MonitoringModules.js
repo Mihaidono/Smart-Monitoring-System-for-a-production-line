b798348c98f99e613f92d4a7f7b887b1c42d4019
Mihaidono <amihai2001@gmail.com>
2024-04-11 11:27:32 +0300

    added monitoring modules

---
+++ Frontend/smart-monitoring-system-rui/src/config/enums/MonitoringModules.js
@@ -0,0 +1,7 @@
+export const MonitoringModules = {
+  HOME: [0.102, -0.064],
+  WAREHOUSE: [0.995, -0.242],
+  PROCESSING_STATION: [0.33, -0.242],
+  SORTING_LINE: [-0.229, -0.358],
+  SHIPPING: [-0.117, -0.135],
+};
