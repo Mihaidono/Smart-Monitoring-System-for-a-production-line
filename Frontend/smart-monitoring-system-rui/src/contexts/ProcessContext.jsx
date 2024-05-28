b3896d0eebc6735a32836bc96e4d0920a03dc023
Mihaidono <amihai2001@gmail.com>
2024-05-28 17:42:27 +0300

    reverted changes to process and replaced it with localStorage for processedState

--- Frontend/smart-monitoring-system-rui/src/contexts/ProcessContext.jsx
+++ Frontend/smart-monitoring-system-rui/src/contexts/ProcessContext.jsx
@@ -1,14 +1,17 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useState } from "react";
 import { AvailableURLs } from "../config/enums/AvailableURLs";
 import axios from "axios";
 
 export const ProcessContext = createContext();
 
 export const ProcessProvider = ({ children }) => {
-  const [processStarted, setProcessStarted] = useState(false);
+  const [processStarted, setProcessStarted] = useState(
+    JSON.parse(localStorage.getItem("processStarted")) || false
+  );
 
   const updateProcessStarted = (newValue) => {
     setProcessStarted(newValue);
+    localStorage.setItem("processStarted", JSON.stringify(newValue));
     const sendStateToBackend = async () => {
       try {
         await axios.post(`${AvailableURLs.BACKEND_HTTP}/set_process_state`, {
@@ -19,22 +22,6 @@ export const ProcessProvider = ({ children }) => {
 
     sendStateToBackend();
   };
-
-  // attempt at updating process state at init for no discrepancies
-
-  useEffect(() => {
-    const sendStateToBackend = async () => {
-      try {
-        await axios.post(`${AvailableURLs.BACKEND_HTTP}/set_process_state`, {
-          new_state: processStarted,
-        });
-      } catch (error) {}
-    };
-
-    sendStateToBackend();
-  }, []);
-
-  //----------------------------------------------------------------------
 
   return (
     <ProcessContext.Provider value={{ processStarted, updateProcessStarted }}>
