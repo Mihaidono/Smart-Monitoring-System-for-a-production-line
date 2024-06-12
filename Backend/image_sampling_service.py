a46a5da8a2eb1c539e62e03235f6e57dd9014d27
Mihaidono <amihai2001@gmail.com>
2024-06-12 17:09:02 +0300

    changed sampling service output dir and added credentials to broker connection

--- Backend/image_sampling_service.py
+++ Backend/image_sampling_service.py
@@ -17,10 +17,12 @@ previous_img = None
 previous_timestamp = datetime.utcnow()
 is_img_processing = False
 threshold = float(os.getenv("SIMILARITY_THRESHOLD"))
+output_folder = "sampled_images_empty_storage"
+os.mkdir(output_folder)
 
 
 def decode_image_from_base64(json_message: dict) -> cv2.typing.MatLike:
-    image_data = base64.b64decode(strip_encoded_image_data(json_message['data']))
+    image_data = base64.b64decode(strip_encoded_image_data(json_message["data"]))
     image_np = np.frombuffer(image_data, np.uint8)
     return cv2.imdecode(image_np, cv2.IMREAD_COLOR)
 
@@ -38,12 +40,14 @@ def sample_images_by_user_input(json_content: dict):
             diff = cv2.absdiff(img, previous_img)
             gray_diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
             if np.sum(gray_diff) >= threshold:
-                hashed_image_timestamp = hashlib.sha256(json_content["ts"].encode()).hexdigest()
+                hashed_image_timestamp = hashlib.sha256(
+                    json_content["ts"].encode()
+                ).hexdigest()
                 previous_img = np.copy(img)
-                cv2.imshow(f'Image {hashed_image_timestamp} ', img)
+                cv2.imshow(f"Image {hashed_image_timestamp} ", img)
                 key_pressed = cv2.waitKey(0)
-                if key_pressed == ord('s'):
-                    cv2.imwrite(f'sampled_images/{hashed_image_timestamp}.jpg', img)
+                if key_pressed == ord("s"):
+                    cv2.imwrite(f"{output_folder}/{hashed_image_timestamp}.jpg", img)
                     print(f"Successfully saved {hashed_image_timestamp}.jpg")
                 cv2.destroyAllWindows()
             else:
@@ -56,12 +60,14 @@ def sample_images_automatically(json_content: dict):
     global is_img_processing
     is_img_processing = True
     if "data" in json_content:
-        image_data = base64.b64decode(strip_encoded_image_data(json_content['data']))
+        image_data = base64.b64decode(strip_encoded_image_data(json_content["data"]))
         image_np = np.frombuffer(image_data, np.uint8)
         img = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
         if img is not None:
-            hashed_image_timestamp = hashlib.sha256(json_content["ts"].encode()).hexdigest()
-            cv2.imwrite(f'sampled_images/{hashed_image_timestamp}.jpg', img)
+            hashed_image_timestamp = hashlib.sha256(
+                json_content["ts"].encode()
+            ).hexdigest()
+            cv2.imwrite(f"{output_folder}/{hashed_image_timestamp}.jpg", img)
             print(f"Successfully saved image {hashed_image_timestamp}")
             time.sleep(sampling_period)
             is_img_processing = False
@@ -70,25 +76,28 @@ def sample_images_automatically(json_content: dict):
 
 
 def strip_encoded_image_data(image_encoded_json_data: str) -> str:
-    return image_encoded_json_data.split(',', 1)[-1].strip()
+    return image_encoded_json_data.split(",", 1)[-1].strip()
 
 
 def on_connect_txt(client, userdata, flags, rc):
     if rc == 0:
-        print(f"Successfully connected client {client_txt_name} to TXT Controller")
         client.connected_flag = True
-        client.subscribe('i/cam')
+        client.subscribe("i/cam")
+        print(f"Successfully connected client {client_txt_name} to TXT Controller")
 
 
 def on_message_txt(client, userdata, msg):
     json_message = json.loads(msg.payload)
     if "ts" in json_message:
-        current_timestamp = datetime.strptime(json_message["ts"], "%Y-%m-%dT%H:%M:%S.%fZ")
+        current_timestamp = datetime.strptime(
+            json_message["ts"], "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
         if previous_timestamp + timedelta(seconds=1) <= current_timestamp:
             if is_sampling_automated:
                 if not is_img_processing:
-                    automatic_sampling_thread = threading.Thread(target=sample_images_automatically,
-                                                                 args=(json_message,))
+                    automatic_sampling_thread = threading.Thread(
+                        target=sample_images_automatically, args=(json_message,)
+                    )
                     automatic_sampling_thread.start()
             else:
                 sample_images_by_user_input(json_message)
@@ -104,19 +113,22 @@ sampling_period = float(os.getenv("SAMPLING_PERIOD"))
 txt_broker_address = os.getenv("TXT_CONTROLLER_ADDRESS")
 port_used = int(os.getenv("TXT_CONTROLLER_PORT_USED"))
 keep_alive = int(os.getenv("TXT_CONTROLLER_KEEP_ALIVE"))
+username = os.getenv('TXT_USERNAME')
+passwd = os.getenv('TXT_PASSWD')
 
 client_txt_name = "ImageSamplingService"
 client_txt = mqtt.Client(client_id=client_txt_name)
 client_txt.on_connect = on_connect_txt
 client_txt.on_message = on_message_txt
 client_txt.on_disconnect = on_disconnect
+client_txt.username_pw_set(username=username, password=passwd)
 
 try:
     client_txt.connect(host=txt_broker_address, port=port_used, keepalive=keep_alive)
     client_txt.loop_forever()
 except TimeoutError as ex:
-    print(f'{client_txt_name} failed to connect to TXT: {ex}')
+    print(f"{client_txt_name} failed to connect to TXT: {ex}")
     client_txt.disconnect()
 except Exception as ex:
-    print(f'{client_txt_name} failed to continue because of {ex}')
+    print(f"{client_txt_name} failed to continue because of {ex}")
     client_txt.disconnect()
